640f97f419a7e4743967d068b4ecd8e29b9ace71
SonPham <phamngocsonl13.cla@gmail.com>
2019-06-05 15:26:07 +0700

    Tạo footer
    Người đăng: Phạm Ngọc Sơn
    Nhiệm vụ: Tạp Footer cho trang web

---
+++ PhamNgocSonCV.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("#icon-footer-facebook").click(function(event) {
+			
+		window.open('https://www.facebook.com/sonpham1602')
+	});
+	$("#icon-footer-github").click(function(event) {
+
+		window.open('https://github.com/SonPham1602')
+	});
+	$("#icon-footer-linkedin").click(function(event) {
+
+		window.open('https://www.linkedin.com/in/sonpham1602/')
+	});
+	$("#icon-footer-youtube").click(function(event) {
+
+		window.open('https://www.youtube.com/channel/UCSg0LTeHaCA3VMyQiErs1dw')
+	});
+
+
+});
